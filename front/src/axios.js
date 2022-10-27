dbca9d0dbc6c903b4de032a5a09b7024c84c6d04
minseung Kim <shvdl12@gmail.com>
2022-10-27 23:31:29 +0900

    Feat custom axios

---
+++ front/src/axios.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import getToken from './utils/token'
+
+const _axios = axios.create()
+
+
+_axios.interceptors.request.use(
+  function(config) {
+    // Do something before request is sent
+    config.headers.Authorization = getToken()
+
+    return config;
+  },
+  function(error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+_axios.interceptors.response.use(// 확인해
+  function(response) {
+    // Do something with response data
+    // if(response.data.token) {
+    //   localStorage.accessTokenAdmin = response.data.token;
+    //   axios.defaults.headers.common.Authorization = localStorage.accessTokenAdmin;
+    //   store.commit("RESET")
+    //   console.log(store.state)
+      
+    // }
+    return Promise.resolve(response);
+  },
+  function(error) {
+    // Do something with response error
+    if(error.response.status === 401) {
+      window.location.href = '/';
+     // delete localStorage
+    }
+    else if(error.response.status === 500 ) {
+      alert(error)
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default _axios;
